006845c582828aade2f92d23f7b4be6784324c86
Ilia Egorov <i.egorov119@yandex.ru>
2023-05-17 22:34:32 +0400

    error processing

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -44,18 +44,18 @@ const createUser = (req, res) => {
 }
 
 const updateUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about, avatar })
-    .then(user => res.status(200).send(user))
-    .catch(err => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: "Некорректные данные пользователя" })
-      } else if (err.status === 404) {
-        res.status(404).send({ message: err.message })
-      } else {
-        res.status(500).send({ message: "Ошибка сервера" })
-      }
-    });
+  const { _id, name, about, avatar } = req.body;
+  User.findByIdAndUpdate(_id, { name, about, avatar })
+  .then(user => res.status(200).send(user))
+  .catch(err => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: "Некорректные данные пользователя" })
+    } else if (err.status === 404) {
+      res.status(404).send({ message: err.message })
+    } else {
+      res.status(500).send({ message: "Ошибка сервера" })
+    }
+  });
 }
 
 const updateAvatar = (req, res) => {
