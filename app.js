3fcf4d64a62713ddcd9cb01ca9dfc1b944848732
Ilia Egorov <i.egorov119@yandex.ru>
2023-05-20 10:41:17 +0400

    error processing

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes/index')
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 
 const app = express();
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-    _id: '64359344f4f4aa99df69f780'
+    _id: "64359344f4f4aa99df69f780",
   };
   next();
 });
 app.use(routes);
 
+app.all("*", (req, res) => {
+  res.status(404).send({ message: "Страница не найдена" });
+});
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`);
 });
